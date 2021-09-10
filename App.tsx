223f6f70c3e0d58eaeb2b8307b22c48837332395
Joice Gabrielly Carmona <joice.carmona@estudante.ifms.edu.br>
2021-09-10 13:45:22 -0300

    cabeçalho finalizado

--- App.tsx
+++ App.tsx
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
         <View style={styles.header}>
-            <Image source={require("./assets/menor.png")}></Image>
+            <Image style={styles.botao} source={require("./assets/menor.png")}></Image>
 
-            <Image source={require("./assets/lupa.png")}></Image>
+        <View style={styles.fundopesquisa}>
+            <Image style={styles.botaopesquisa} source={require("./assets/lupa.png")}></Image>
 
-            <Image source={require("./assets/compartilhar.png")}></Image>
+                <TextInput style={styles.textInput} placeholder="Pesquisa"
+                placeholderTextColor="#F5FFFF">
+
+                </TextInput>
+        </View>
+
+            <Image style={styles.botao} source={require("./assets/compartilhar.png")}></Image>
         </View>
 
       <StatusBar style="auto" />
@@ -27,8 +34,44 @@ const styles = StyleSheet.create({
   },
 
   header: {
-    minHeight: 30,
+    minHeight: 70,
     backgroundColor: '#4369B0',
-    paddingTop: 30,
-  } 
+    paddingTop: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+
+  botao: {
+    height: 32,
+    width: 32,
+    marginRight: 10,
+    marginLeft: 10,
+  },
+
+  botaopesquisa: {
+    height: 15,
+    width: 15,
+    marginLeft: 10,
+  },
+
+  fundopesquisa: {
+    backgroundColor: '#2C4877',
+    borderRadius: 28,
+    flex: 1,
+    marginHorizontal: 10,
+    height: 35,
+    alignItems: 'center',
+    flexDirection: 'row',
+
+  },
+
+  textInput: {
+    flex: 1,
+    paddingRight: 10,
+    paddingLeft: 5,
+    color: "#F5FFFF",
+
+  },
+
 });
